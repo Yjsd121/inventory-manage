b8c48d8ec76507514fe0e88802ff3c5e840f1e6a
Yjsd121 <yadetjsd@gmail.com>
2025-12-23 22:11:13 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { BarNav } from './components/NavegationBar'
+import './App.css'
+// import { useState } from 'react'
+import { BarNav } from './components/Barnav/NavegationBar'
+// import { Inventory } from './components/Show/inventory'
+import elementos from './mocks/Elementos.json'
 
 export function App () {
+  const elementss = elementos.inventory
+  // const [inventory, setinventory] = useState(null)
+  const hasinventory = elementss?.length > 0
   return (
     <main>
       <BarNav />
+      {
+        hasinventory
+          ? (
+              elementss.map(ele => (
+                <li key={ele.id}>
+                  <p>{ele.name}</p>
+                </li>
+              ))
+            )
+          : <h1> No hay inventario</h1>
+      }
     </main>
   )
 }
---
+++ src/components/Barnav/NavegationBar.jsx
@@ -0,0 +1,20 @@
+import './NavegationBar.css'
+
+export function BarNav () {
+  return (
+    <section className='main'>
+      <div>
+        <img src='' />
+      </div>
+      <nav className='nav'>
+        <a>Dashboard</a>
+        <a>Products</a>
+        <a>Categories</a>
+        <a>Orders</a>
+        <a>Users</a>
+        <a>Profile</a>
+        <a>Logoutz</a>
+      </nav>
+    </section>
+  )
+}
