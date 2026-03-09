f76b04e0ac1f3c60f38b0ac32fee47c600d84dc6
Yjsd121 <yadetjsd@gmail.com>
2026-03-08 21:19:56 -0600

    MySQL connection successfully

--- Inventory-Manage-backend/server.js
+++ Inventory-Manage-backend/server.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const MySQL = require('mysql2')
 const fs = require('fs').promises
 const cors = require('cors')
 const path = require('path')
+
+const connection = MySQL.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'yjsd348d',
+  database: 'inventory_manage'
+})
+connection.connect(err => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('MySQL connectión is successfully ')
+  }
+})
 const app = express()
+
 const port = 3000
 
 app.use(cors())
+
+app.get('/test', (req, res) => {
+  connection.query('SELECT * FROM  products', (err, rows, fields) => {
+    if (err) throw err
+    console.log('the solution is: ', rows[0].solutions)
+  })
+
+  connection.end()
+})
 app.get('/Orders', async (req, res) => {
   try {
     const filepath = path.join(__dirname, 'Data', 'Orders.json')
