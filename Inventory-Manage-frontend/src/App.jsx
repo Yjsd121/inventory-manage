0403afd00a5288aa7e255da39df7ab2bfb9639b3
Yjsd121 <yadetjsd@gmail.com>
2026-03-14 17:58:38 -0600

    Improving Sidebar

--- Inventory-Manage-frontend/src/App.jsx
+++ Inventory-Manage-frontend/src/App.jsx
@@ -7,14 +7,13 @@ export function App() {
   const [view, setview] = useState("Dashboard")
   const [selectedfilters, setFilters] = useState({})
   const [SelectedData, setselectedData] = useState({})
-  const items = {}
+  
   useEffect(() => {
     console.log(SelectedData)
   }, [SelectedData])
   return (
     <main>
       <BarNav view={view} setview={setview} />
-
       <Vistas valor={view} SelectedData={SelectedData} setFilters={setFilters} selectedfilters={selectedfilters} setselectedData={setselectedData} />
     </main>
   )
--- Inventory-Manage-frontend/src/components/SideBar/NavegationBar.jsx
+++ Inventory-Manage-frontend/src/components/SideBar/NavegationBar.jsx
@@ -5,27 +5,41 @@ import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
 import GroupIcon from '@mui/icons-material/Group';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useState } from 'react';
 
 export function BarNav({ view, setview }) {
+  const [rotate, setrotate] = useState(true)
+  const [show, setshow] = useState(true)
+
+  console.log(show)
   return (
-    <section className='main'>
+    <section className={show ? 'main' : 'main hidde'}>
       <div className='logo'>
-        <img className='icon' src='/react.svg' />
-        <p>Inv Manage</p>
+        <img className={show ? 'ProfileImg' : 'icon'} src='/react.svg' />
+        <p><span className={show ? "text" : "text hide"}>Inv Manage</span></p>
       </div>
       <nav className='nav'>
 
         <div className='bar'>
           <ul>
-            <li className={view === "Dashboard" ? 'active' : ''} onClick={() => setview("Dashboard")}><DashboardIcon /> Dashboard</li>
-            <li className={view === "Products" ? 'active' : ''} onClick={() => setview("Products")}><Inventory2Icon /> Products</li>
-            <li className={view === "Orders" ? 'active' : ''} onClick={() => setview("Orders")}><AlignHorizontalLeftIcon /> Orders</li>
-            <li className={view === "Users" ? 'active' : ''} onClick={() => setview("Users")}><GroupIcon />Users</li>
-            <li className={view === "Profile" ? 'active' : ''} onClick={() => setview("Profile")}><AccountCircleIcon /> Profile</li>
+            <li className={view === "Dashboard" ? 'active' : ''} onClick={() => setview("Dashboard")}><DashboardIcon /> <span className={show ? "text" : "text hide"}>Dashboard</span></li>
+            <li className={view === "Products" ? 'active' : ''} onClick={() => setview("Products")}><Inventory2Icon /> <span className={show ? "text" : "text hide"}>Products</span></li>
+            <li className={view === "Orders" ? 'active' : ''} onClick={() => setview("Orders")}><AlignHorizontalLeftIcon /> <span className={show ? "text" : "text hide"}>Orders</span></li>
+            <li className={view === "Users" ? 'active' : ''} onClick={() => setview("Users")}><GroupIcon /><span className={show ? "text" : "text hide"}>Users</span></li>
+            <li className={view === "Profile" ? 'active' : ''} onClick={() => setview("Profile")}><AccountCircleIcon /> <span className={show ? "text" : "text hide"}>Profile</span></li>
           </ul>
         </div>
-        <div className='logout'>
-          <li>< LogoutIcon />Logout</li>
+        <div className='logout' >
+          <li>< LogoutIcon /><span className={show ? "text" : "text hide"}>Logout</span></li>
+        </div>
+        <div className='Deployment' onClick={() => {
+          setrotate(!rotate)
+          setshow(!show)
+        }}>
+          <li><ArrowForwardIosIcon
+            className={rotate ? "arrow rotate" : 'arrow no'}
+          /></li>
         </div>
       </nav>
     </section>
