c60b41979668af49e202a3dcc6e3d10e7af4654c
Yjsd121 <yadetjsd@gmail.com>
2026-04-02 22:50:50 -0600

    Orders view improved

--- Inventory-Manage-frontend/src/orders/Orders.jsx
+++ Inventory-Manage-frontend/src/orders/Orders.jsx
@@ -5,16 +5,22 @@ import orderFilterss from '../mocks/Ordersfilters.json'
 
 export function Orderlist({ orderss, selectedfilters }) {
   return (
+
     orderss.filter(order => {
       return ((order.Status === selectedfilters.status || selectedfilters.status === 'all') &&
         (order.Price === selectedfilters.Price || selectedfilters.Price === 'all'))
     }).map(order => (
-      <div key={order.Id} className='order-card'>
-        <h4>{order.Id}</h4>
-        <p>Producto: {order.Product}</p>
-        <p>Cantidad: {order.Quantity}</p>
-        <p>Estado: {order.Status}</p>
-        <button> View </button>
+      <div key={order.Id} className="order-card">
+        <span>{order.Id}</span>
+        <span>{order.Product}</span>
+        <span>{order.Quantity}</span>
+        <span>${order.Price}</span>
+
+        <span className={`status ${order.Status.toLowerCase()}`}>
+          {order.Status}
+        </span>
+
+        <button>View</button>
       </div>
     ))
   )
@@ -23,7 +29,17 @@ function Orders({ orderss, selectedfilters }) {
   const hasorders = orderss?.length > 0
   return (
     hasorders
-      ? <Orderlist orderss={orderss} selectedfilters={selectedfilters} />
+      ? <div>
+        <div className="orders-header">
+          <span>Order #</span>
+          <span>Product</span>
+          <span>Quantity</span>
+          <span>Total</span>
+          <span>Status</span>
+          <span>---------------------------------</span>
+        </div>
+        <Orderlist orderss={orderss} selectedfilters={selectedfilters} />
+      </div>
       : (
         <h2>No hay órdenes</h2>
       )
@@ -34,17 +50,12 @@ export function ShowOrders({ endpoint }) {
   const { selectedfilters, SelectedData } = useAppContext();
   return (
     <section className='orders'>
-      <h2>Pending Orders</h2>
-      <div className='color'>
-        <SearchBar
-          endpoint={endpoint}
-          filters={orderFilterss.orderFilters}
-        />
-
-        <div className='ShowElements Orders'>
-          <Orders orderss={SelectedData} selectedfilters={selectedfilters} />
-        </div>
-        
+      <SearchBar
+        endpoint={endpoint}
+        filters={orderFilterss.orderFilters}
+      />
+      <div className='ShowElements Orders'>
+        <Orders orderss={SelectedData} selectedfilters={selectedfilters} />
       </div>
     </section>
   )
