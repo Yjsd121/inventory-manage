ec6c2fd0db24b736e868b43a062b039f6298aeb5
Yader Sáenz <yadetjsd@gmail.com>
2026-06-08 23:29:31 -0600

    ident fixed

--- Inventory-Manage-backend/services/Auth.service.js
+++ Inventory-Manage-backend/services/Auth.service.js
@@ -1,8 +1,6 @@
 const Query = require('../utils/Query')
 
-exports.getusers = async (
-  email
-) => {
+exports.getusers = async (email) => {
   return await Query('SELECT * FROM users WHERE User_email = ?', [email])
 }
 
